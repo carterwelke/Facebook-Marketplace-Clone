3bae092f0a3b1a56a00d762215f575e7d357f76d
Carter Welke <carterwelke@gmail.com>
2021-11-29 13:12:09 -0800

    Added code from authenticated books login example

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,9 +3,75 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 // import IconButton from '@mui/material/IconButton';
 // import MenuIcon from '@mui/icons-material/Menu';
+
+import {useHistory} from 'react-router-dom';
+
+/**
+ * Login authentication from authenticated books example
+ *
+ * @return {object} JSX
+ */
+function Login() {
+  const [user, setUser] = React.useState({email: '', password: ''});
+  const history = useHistory();
+
+  const handleInputChange = (event) => {
+    const {value, name} = event.target;
+    const u = user;
+    u[name] = value;
+    setUser(u);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch('/authenticate', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw res;
+        }
+        return res.json();
+      })
+      .then((json) => {
+        localStorage.setItem('user', JSON.stringify(json));
+        history.push('/');
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('Error logging in, please try again');
+      });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={handleInputChange}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={handleInputChange}
+        required
+      />
+      <input type="submit" value="Login"/>
+    </form>
+  );
+}
+
+// export default Login;
 
 /**
  * Simple appbar with login button
@@ -21,9 +87,10 @@ export default function ButtonAppBar() {
           <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
             Facebook Marketplace
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Login/>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
