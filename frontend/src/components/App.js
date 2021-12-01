b99f8f863af7a6602d54f06dccde908260124cba
itemiral <itemiral@ucsc.edu>
2021-11-30 18:09:12 -0800

    adding homepage.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import Homepage from ' /Homepage';
+//  import Login from './Login';
 
-import Dummy from './Dummy';
-import LoginScreen from './Login';
-import CreateUserScreen from './CreateUser';
-import CategoryScreen from './Category';
-import {BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
-// import {AppBar} from '@mui/material';
+//  import Dummy from './Dummy';
 
 /**
  * Simple component with no state.
@@ -14,28 +11,7 @@ import {BrowserRouter, Route, NavLink, Switch} from 'react-router-dom';
  */
 function App() {
   return (
-    <BrowserRouter>
-      <ul className="navigation">
-        <li><NavLink exact to="/">Home</NavLink></li>
-        <li><NavLink to="/Login">Login</NavLink></li>
-        <li><NavLink to="/CreateUser">Create Account</NavLink></li>
-        <li><NavLink to="/Categories">Categories</NavLink></li>
-      </ul>
-      <Switch>
-        <Route path="/" exact>
-          <Dummy/>
-        </Route>
-        <Route path="/Login">
-          <LoginScreen/>
-        </Route>
-        <Route path="/CreateUser">
-          <CreateUserScreen/>
-        </Route>
-        <Route path="/Categories">
-          <CategoryScreen/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Homepage />
   );
 }
 
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,102 @@
+import React from 'react';
+//  import ReactDOM from 'react-dom';
+import {
+  Toolbar, AppBar, Typography,
+  Button, Grid, Fab, Box, InputBase,
+} from '@mui/material';
+import {
+  makeStyles,
+} from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    bgcolor: 'background.paper',
+  },
+  search: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+/**
+ * Simple component with no state.
+ *
+ * @return {object} JSX
+ */
+function Homepage() {
+  const classes = useStyles();
+  return (
+    <div>
+      <Box className={classes.box}
+        sx={{
+          position: 'sticky',
+          p: 2,
+          m: 2,
+        }}
+      >
+        <AppBar>
+          <Toolbar>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Grid item>
+                <Typography variant="h6">
+                  Facebook
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained">Log In</Button>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box
+        className={classes.box}
+        sx={{
+          position: 'absolute',
+          p: 2,
+          m: -2,
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid item xs="auto">
+            <Fab variant="extended">Sell</Fab>
+          </Grid>
+          <Grid item xs="auto">
+            <Fab variant="extended">All Categories</Fab>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box className={classes.box}
+        sx={{
+          position: 'absolute',
+          left: '-55px',
+          p: 4,
+          m: 4,
+        }}
+      >
+        <Grid>
+          <Grid item
+            sx={{
+              border: 1,
+              borderRadius: '8px',
+            }}>
+            <div className={classes.search}>
+              <SearchIcon />
+              <InputBase placeholder=" Search" />
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
+export default Homepage;
