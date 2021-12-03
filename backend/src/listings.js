d0ae94deb24f732bd5ee91545a4e2d257582ba79
Carter Welke <carterwelke@gmail.com>
2021-12-03 13:05:57 -0800

    backend testing for listings

--- backend/src/listings.js
+++ backend/src/listings.js
@@ -4,9 +4,9 @@ exports.getAll = async (req, res) => {
     //get all listings from database
     // console.log("here?");
     const listings = await db.getAllListings();
-    if(listings.length === 0) {
+    /* if(listings.length === 0) {
         res.status(404).send();
-    }
+    } */
     // console.log(listings);
     res.status(200).json(listings);
 }
