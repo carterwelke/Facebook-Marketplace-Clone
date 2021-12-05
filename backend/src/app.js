a4fb520fda4f6e8436cf285ea1a76d13c5cce95d
itemiral <itemiral@ucsc.edu>
2021-12-04 16:52:25 -0800

    adding dummy for testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use(
   }),
 );
 
-// app.get('/v0/dummy', dummy.get);
+app.get('/v0/dummy', dummy.get);
 // Your routes go here
 app.post('/authenticate', auth.authenticate);
 app.post('/createUser', auth.createUser);
